e1de408db1221b0e917e2fb44c979bec1a48620e
Julien EMMANUEL <JuEm0406@gmail.com>
2015-05-14 18:44:32 +0200

    .

--- src/BatailleNavale.java
+++ src/BatailleNavale.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 /**
  * Cette classe définit l'objet "bateau".
  * @author Julien EMMANUEL, Charlotte RICHAD, Thomas DUTOUR, Alexis SAGET
  */
 public class BatailleNavale {
-
-	public static void main(String[] args) {
-
+	
+	public static void main(String[] args) 
+	{
+		genererBateaux();
 	}
-
+	
+	public static void genererBateaux ()
+	{
+		Bateau[] bateaux = new Bateau[5];
+		bateaux[0] = new Bateau("Porte-avions",5);
+		bateaux[1] = new Bateau("Croiseur",4);
+		bateaux[2] = new Bateau("Contre-torpilleur",3);
+		bateaux[3] = new Bateau("Sous-marin",3);
+		bateaux[4] = new Bateau("Torpilleur",2);
+		positionner(bateaux);
+		for (int i = 0; i<5; i++)
+		{
+			System.out.println(bateaux[i]);
+		}
+	}
+	public static void positionner(Bateau[] bateaux)
+	{
+		bateaux[0].position(1,3,true);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un mot :");
+		String str = sc.nextLine();
+	}
 }
--- src/Bateau.java
+++ src/Bateau.java
@@ -3,9 +3,36 @@
  * @author Julien EMMANUEL, Charlotte RICHAD, Thomas DUTOUR, Alexis SAGET
  */
 public class Bateau {
-
-	public Bateau() {
-
+	private String nom;
+	private int taille;
+	private boolean[] etat;
+	private int vie;
+	private boolean vertical;
+	private int positionX;
+	private int positionY;
+	public Bateau(String nom, int taille)
+	{
+		this.nom = nom;
+		this.taille = taille;
+		this.vie = this.taille;
+		this.vertical = false;
+		this.positionX = 0;
+		this.positionY = 0;
+		this.etat = new boolean[taille];
+		for(int i = 0; i<etat.length; i++)
+		{
+			etat[i] = true;
+		}
 	}
-
+	public String toString()
+	{
+		String chaine = nom + " - taille = " + taille + " - position = {" + positionX + ";" + positionY + "}";
+		return chaine;
+	}
+	public void position(int positionX, int positionY, boolean vertical)
+	{
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.vertical = vertical;
+	}
 }
